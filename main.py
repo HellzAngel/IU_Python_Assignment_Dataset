41a445402152405cb74621eea38d031f16f705a1
HellzAngel <itmeanuhere01@gmail.com>
2022-07-11 20:43:25 +0530

    fixed bugs

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-# Lets import the neccesary libraries to complete the assignment 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt #to plot with matplotlib
-import unittest #for unit testing
+import unittest 
 import os
-#for mathematical operations
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
-#to plot with Bokeh
 from bokeh.io import output_file
 from bokeh.plotting import figure,show
 from bokeh.layouts import gridplot
-from scipy.optimize import curve_fit #curve fitting
 from pylab import * 
 from sqlalchemy import *
 
+#reading files using pandas
 Train_data = pd.read_csv('train.csv') 
 Ideal_data = pd.read_csv('ideal.csv') 
 Test_data = pd.read_csv('test.csv')
@@ -25,50 +21,53 @@ Train_Y2 = Train_data['y2']
 Train_Y3 = Train_data['y3']
 Train_Y4 = Train_data['y4']
 
-#Using matplotlib to plot the training data
-fig, axs = plt.subplots(2,2, figsize=(15,15)) # generating the different plots (subplots) at once
-
-#'tight_layout' helps adjusting subplots so they fit into the figure area, 'pad' controls the padding around the figure border & subplots
-fig.tight_layout(pad=3)
-
-# Plotting 4 figures, the 'axs[value,value]' allows to place graphs in the desire position
-axs[0,0].plot(Train_X, Train_Y1)  
-axs[0,0].set_title('Train_X Vs Train_Y1',)
-axs[1,0].plot(Train_X, Train_Y2, 'tab:red')
-axs[1,0].set_title('Train_X Vs Train_Y2')
-axs[0, 1].plot(Train_X, Train_Y3, 'tab:orange')
-axs[0, 1].set_title('Train_X Vs Train_Y3')
-axs[1,1 ].plot(Train_X, Train_Y4, 'tab:green')
-axs[1, 1].set_title('Train_X Vs Train_Y4')
-plt.show()
-
-#using matplotlib to plot the test data
-
-plt.scatter(Test_data['x'], Test_data['y'], color='b')
-plt.suptitle('Test X vs Test Y')
-plt.show()
-
 #Using Bokeh to plot the training data
 
 from bokeh.io import curdoc # importing curdoc to change the theme colour 
 from bokeh.plotting import figure,show
-curdoc().theme = 'night_sky'
+curdoc().theme = 'contrast'
 
 # 4 plots will be display
-b_graph1=figure(width=600, height=600,title='X vs Train_Y1 ') #1st graph for X vs Y1
-b_graph1.triangle(Train_X,Train_Y1, size=12, color='cyan', alpha=0.5)
-b_graph2=figure(width=600, height=600,title='X vs Train_Y3 ')#2nd graph for X vs Y3
-b_graph2.circle(Train_X,Train_Y3,size=12, color='yellow', alpha=0.5)
-b_graph3=figure(width=600, height=600,title='X vs Train_Y2')#3rd graph for X vs Y2
-b_graph3.circle(Train_X,Train_Y2,size=12, color='red', alpha=0.5)
-b_graph4=figure(width=600, height=600,title='X vs Train_Y4 ')#4th graph for X vs Y4
-b_graph4.circle(Train_X,Train_Y4,size=12, color='green', alpha=0.5)
+#plot 1
+train_graph1=figure(width=400, height=400,title='Train_X vs Train_Y1 ') 
+train_graph1.star(Train_X,Train_Y1, size=8, color='indigo', alpha=0.5)
+train_graph1.outline_line_color='navy'
+train_graph1.outline_line_width = 7
+train_graph1.outline_line_alpha = 0.3
+#plot 2
+train_graph2=figure(width=400, height=400,title='Train_X vs Train_Y3 ')
+train_graph2.star(Train_X,Train_Y3,size=8, color='purple', alpha=0.5)
+train_graph2.outline_line_color='navy'
+train_graph2.outline_line_width = 7
+train_graph2.outline_line_alpha = 0.3
+#plot 3
+train_graph3=figure(width=400, height=400,title='Train_X vs Train_Y2')
+train_graph3.star(Train_X,Train_Y2,size=8, color='red', alpha=0.5)
+train_graph3.outline_line_color='navy'
+train_graph3.outline_line_width = 7
+train_graph3.outline_line_alpha = 0.3
+#plot 4
+train_graph4=figure(width=400, height=400,title='Train_X vs Train_Y4 ')
+train_graph4.star(Train_X,Train_Y4,size=8, color='green', alpha=0.5)
+train_graph4.outline_line_color='navy'
+train_graph4.outline_line_width = 7
+train_graph4.outline_line_alpha = 0.3
 
-#grouping all plots in a gridformat following 'p = gridplot([[s1, s2], [s3, s3]])'
-p= gridplot([[b_graph1,b_graph2],[b_graph3,b_graph4]])
-output_file("foo.html")
+#ploating in grid format
+p= gridplot([[train_graph1,train_graph2],[train_graph3,train_graph4]])
+output_file("plots.html")
+curdoc().theme = 'caliber'
 #display plots
 show(p)
+
+# Plotting test data in Bokeh
+test_graph= figure(width=400, height=400,title="Test X vs Test Y")
+test_graph.circle_dot(Test_data['x'], Test_data['y'], size=6, color='red',alpha=0.5 )
+test_graph.outline_line_color='navy'
+test_graph.outline_line_width = 7
+test_graph.outline_line_alpha = 0.3
+output_file("test_plot.html")
+show(test_graph)
 
 #The function 'np.polyfit()' is use to find the least square polynomial fit
 Model_A = np.polyfit(Train_X, Train_Y1, 3)
@@ -98,7 +97,7 @@ mae_y2 = mean_absolute_error(Train_Y2, Prediction_2(Train_X))
 mae_y3 = mean_absolute_error(Train_Y3, Prediction_3(Train_X))
 mae_y4 = mean_absolute_error(Train_Y4, Prediction_4(Train_X))
 
-
+print("--------------------------------------------")
 print("Mean Absolute Error (MAE)")
 print("Model 1 :", mae_y1)
 print("Model 2 :", mae_y2)
@@ -112,11 +111,13 @@ mse_y2 = mean_squared_error(Train_Y2, Prediction_2(Train_X))
 mse_y3 = mean_squared_error(Train_Y3, Prediction_3(Train_X))
 mse_y4 = mean_squared_error(Train_Y4, Prediction_4(Train_X))
 
+print("--------------------------------------------")
 print("Mean Squared Error (MSE)")
 print("Model 1 :", mse_y1)
 print("Model 2 :", mse_y2)
 print("Model 3 :", mse_y3)
 print("Model 4 :", mse_y4)
+print("--------------------------------------------")
 
 # R2 Scores= Coefficient of determination
 r2_y1 = r2_score(Train_Y1, Prediction_1(Train_X))
@@ -129,48 +130,7 @@ print("Model 1 :", r2_y1)
 print("Model 2 :", r2_y2)
 print("Model 3 :", r2_y3)
 print("Model 4 :", r2_y4)
-
-
-
-# to meassure erro and or performance it is important that we use these:
-
-# MAE=mean absolute error
-mae_y1 = mean_absolute_error(Train_Y1, Prediction_1(Train_X))
-mae_y2 = mean_absolute_error(Train_Y2, Prediction_2(Train_X))
-mae_y3 = mean_absolute_error(Train_Y3, Prediction_3(Train_X))
-mae_y4 = mean_absolute_error(Train_Y4, Prediction_4(Train_X))
-
-
-print("Mean Absolute Error (MAE)")
-print("Model 1 :", mae_y1)
-print("Model 2 :", mae_y2)
-print("Model 3 :", mae_y3)
-print("Model 4 :", mae_y4)
 print("--------------------------------------------")
-
-# MSE= mean squared error
-mse_y1 = mean_squared_error(Train_Y1, Prediction_1(Train_X))
-mse_y2 = mean_squared_error(Train_Y2, Prediction_2(Train_X))
-mse_y3 = mean_squared_error(Train_Y3, Prediction_3(Train_X))
-mse_y4 = mean_squared_error(Train_Y4, Prediction_4(Train_X))
-
-print("Mean Squared Error (MSE)")
-print("Model 1 :", mse_y1)
-print("Model 2 :", mse_y2)
-print("Model 3 :", mse_y3)
-print("Model 4 :", mse_y4)
-
-# R2 Scores= Coefficient of determination
-r2_y1 = r2_score(Train_Y1, Prediction_1(Train_X))
-r2_y2 = r2_score(Train_Y2, Prediction_2(Train_X))
-r2_y3 = r2_score(Train_Y3, Prediction_3(Train_X))
-r2_y4 = r2_score(Train_Y4, Prediction_4(Train_X))
-print("--------------------------------------------")
-print("R2 Score for models is as follows")
-print("Model 1 :", r2_y1)
-print("Model 2 :", r2_y2)
-print("Model 3 :", r2_y3)
-print("Model 4 :", r2_y4)
 
 #Finging the ideal functions 
 def ideal_function(train_data, ideal_data):
@@ -194,7 +154,7 @@ print("Ideal Function (Y1) is:", "y" + str(Ideal_Y1[0]),"," ' Ideal Function (Y2
 print("Ideal Function (Y3) is:", "y" + str(Ideal_Y3[0]), "," ' Ideal Function (Y4) is:', "y" + str(Ideal_Y4[0]))
 
 
-#find maximum deviation aka criteria ii
+#find maximum deviation  criteria ii
 def maximum_deviation(train_data, ideal_data):
    
     dev = abs(train_data.values - ideal_data.values)
@@ -257,7 +217,7 @@ class databaseconnection:
     def save_trainingdata(self):
        
         training = pd.read_csv('train.csv')
-        sqlite_table = "training_data"
+        sqlite_table = "training_data_table"
         try:
             sqlite_connection = self.engine.connect()
             training.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
@@ -270,7 +230,7 @@ class databaseidealconnection(databaseconnection):
     def save_idealfunctions(self):
     
         ideal= pd.read_csv('ideal.csv')     #saving ideal functions data to SQLite file
-        sqlite_table = "ideal_functions"
+        sqlite_table = "ideal_functions_table"
         try:
             sqlite_connection = self.engine.connect()
             ideal.to_sql(sqlite_table, sqlite_connection, if_exists='fail')
@@ -308,12 +268,12 @@ class InsertionTestCheck(unittest.TestCase):
         test_data_table = Table('test_data_table', metadata, autoload=True)
         query = test_data_table.select()
 
-        df_test = pd.DataFrame(query.execute())
-        df_test.drop(0, axis=1, inplace=True)
-        df_test.reset_index(drop=True, inplace=True)
-        df_test.columns = Test_data.columns
-        self.assertEqual(len(df_test.columns), len(Test_data.columns))
-        pd.testing.assert_frame_equal(df_test, Test_data)
+        data_test = pd.DataFrame(query.execute())
+        data_test.drop('index', axis=1, inplace=True)    
+        data_test.reset_index(drop=True, inplace=True)
+        data_test.columns = Test_data.columns
+        self.assertEqual(len(data_test.columns), len(Test_data.columns))
+        pd.testing.assert_frame_equal(data_test, Test_data)
         
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
@@ -345,25 +305,27 @@ if __name__ == '__main__':
 
 #4 plots:
 
-i_graph1 = figure(width=600, plot_height=600,title="X vs Y1")
-i_graph1.triangle_pin(Ideal_X, Ideal_data['y' + str(Ideal_Y1[0])], size=10, color='cyan', alpha=0.5)
-i_graph1.outline_line_color='yellow' #adding colour to the outsine line of the plot
-i_graph2 = figure(width=600, plot_height=600,title="X vs Y2")
-i_graph2.circle_dot(Ideal_X, Ideal_data['y' + str(Ideal_Y2[0])], size=10, color='cyan', alpha=0.5)
-i_graph2.outline_line_color='yellow'#adding colour to the outsine line of the plot
-i_graph3 = figure(width=600, plot_height=600,title="X vs Y3")
-i_graph3.square_dot(Ideal_X, Ideal_data['y' + str(Ideal_Y3[0])], size=10, color='cyan', alpha=0.5)
-i_graph3.outline_line_color='yellow'#adding colour to the outsine line of the plot
-i_graph4 = figure(width=600, plot_height=600,title="X vs Y4")
-i_graph4.triangle_dot(Ideal_X, Ideal_data['y' + str(Ideal_Y4[0])], size=10, color='cyan', alpha=0.5)
-i_graph4.outline_line_color='yellow'#adding colour to the outsine line of the plot
+i_graph1 = figure(width=400, plot_height=400,title="X vs Y1")
+i_graph1.triangle_pin(Ideal_X, Ideal_data['y' + str(Ideal_Y1[0])], size=6, color='red', alpha=0.5)
+i_graph1.outline_line_color='navy'
+i_graph1.outline_line_width = 7
+i_graph1.outline_line_alpha = 0.3
+i_graph2 = figure(width=400, plot_height=400,title="X vs Y2")
+i_graph2.circle_dot(Ideal_X, Ideal_data['y' + str(Ideal_Y2[0])], size=6, color='red', alpha=0.5)
+i_graph2.outline_line_color='navy'
+i_graph2.outline_line_width = 7
+i_graph2.outline_line_alpha = 0.3
+i_graph3 = figure(width=400, plot_height=400,title="X vs Y3")
+i_graph3.square_dot(Ideal_X, Ideal_data['y' + str(Ideal_Y3[0])], size=6, color='red', alpha=0.5)
+i_graph3.outline_line_color='navy'
+i_graph3.outline_line_width = 7
+i_graph3.outline_line_alpha = 0.3
+i_graph4 = figure(width=400, plot_height=400,title="X vs Y4")
+i_graph4.triangle_dot(Ideal_X, Ideal_data['y' + str(Ideal_Y4[0])], size=6, color='red', alpha=0.5)
+i_graph4.outline_line_color='navy'
+i_graph4.outline_line_width = 7
+i_graph4.outline_line_alpha = 0.3
 # make a grid
 output_file('ideal_functions.html')
 g = gridplot([[i_graph1, i_graph2], [i_graph3, i_graph4]])
 show(g)
-# Plotting test data in Bokeh
-t_graph1= figure(title="Test X vs Test Y")
-t_graph1.circle_dot(Test_data['x'], Test_data['y'], size=12, color='red',alpha=0.5 )
-t_graph1.outline_line_color='yellow' #adding colour to the outsine line of the plot
-output_file("test_data_plotting.html")
-show(t_graph1)
